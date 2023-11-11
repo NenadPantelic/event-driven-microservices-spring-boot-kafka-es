0894c3b7bfa009f43e80170ae80f0910ad11ecb5
NenadPantelic <nenadpantelickg@gmail.com>
2023-11-11 11:59:15 +0100

    Encrypt secrets using jasypt

--- kafka/kafka-producer/src/main/java/com/np/kafka/producer/config/KafkaProducerConfig.java
+++ kafka/kafka-producer/src/main/java/com/np/kafka/producer/config/KafkaProducerConfig.java
@@ -28,6 +28,7 @@ public class KafkaProducerConfig<K extends Serializable, V extends SpecificRecor
     @Bean
     public Map<String, Object> producerConfig() {
         Map<String, Object> props = new HashMap<>();
+        System.out.println("DEBUG - " + kafkaProducerConfigData);
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaConfigData.getBootstrapServers());
         props.put(kafkaConfigData.getSchemaRegistryUrlKey(), kafkaConfigData.getSchemaRegistryUrl());
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, kafkaProducerConfigData.getKeySerializerClass());
---
+++ playground/src/main/java/com/np/TestJasypt.java
@@ -0,0 +1,21 @@
+package com.np;
+
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.jasypt.iv.RandomIvGenerator;
+
+public class TestJasypt {
+
+    public static void main(String[] args) {
+        StandardPBEStringEncryptor pbeStringEncryptor = new StandardPBEStringEncryptor();
+        // key to encrypt secrets; this secret will be needed in runtime to decrypt secrets
+        pbeStringEncryptor.setPassword("Demo_2023!");
+        pbeStringEncryptor.setAlgorithm("PBEWithHMACSHA512AndAES_256");
+        pbeStringEncryptor.setIvGenerator(new RandomIvGenerator());
+
+        String secretToEncrypt = "spring_cloud_password!";
+        String result = pbeStringEncryptor.encrypt(secretToEncrypt);
+        System.out.println(result);
+
+        System.out.println(pbeStringEncryptor.decrypt(result));
+    }
+}
