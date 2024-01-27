1b11dd268f0fa61c9763f6beba5891fb6ff3c8df
NenadPantelic <nenadpantelickg@gmail.com>
2024-01-27 13:52:36 +0100

    Add URI-based API versioning

--- elasticsearch-query-service/src/main/java/com/np/elasticsearch/query/service/api/ElasticsearchDocumentController.java
+++ elasticsearch-query-service/src/main/java/com/np/elasticsearch/query/service/api/ElasticsearchDocumentController.java
@@ -3,6 +3,7 @@ package com.np.elasticsearch.query.service.api;
 import com.np.elasticsearch.query.service.logic.ElasticsearchQueryService;
 import com.np.elasticsearch.query.service.model.ElasticsearchQueryServiceRequestModel;
 import com.np.elasticsearch.query.service.model.ElasticsearchQueryServiceResponseModel;
+import com.np.elasticsearch.query.service.model.ElasticsearchQueryServiceResponseModelV2;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,7 +13,7 @@ import javax.validation.constraints.NotEmpty;
 import java.util.List;
 
 @RestController// @Controller + @ResponseBody -> used to serialized objects to JSON
-@RequestMapping("/api/v1/documents")
+@RequestMapping("/api")
 @Slf4j
 public class ElasticsearchDocumentController {
 
@@ -23,26 +24,45 @@ public class ElasticsearchDocumentController {
     }
 
     // localhost:8183/elasticsearch-query-service/api/v1/documents (context path is changed)
-    @GetMapping
+    @GetMapping("/v1/documents")
     public ResponseEntity<List<ElasticsearchQueryServiceResponseModel>> getAllDocuments() {
         List<ElasticsearchQueryServiceResponseModel> documents = elasticsearchQueryService.getAllDocuments();
         log.info("Elasticsearch returned {} documents", documents.size());
         return ResponseEntity.ok(documents);
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/v1/documents/{id}")
     public ResponseEntity<ElasticsearchQueryServiceResponseModel> getDocumentById(@PathVariable @NotEmpty String id) {
         ElasticsearchQueryServiceResponseModel esDocument = elasticsearchQueryService.getDocumentById(id);
         log.info("Elasticsearch returned document with id {}", id);
         return ResponseEntity.ok(esDocument);
     }
 
-    @PostMapping
+    @GetMapping("/v2/documents/{id}")
+    public ResponseEntity<ElasticsearchQueryServiceResponseModelV2> getDocumentByIdV2(@PathVariable @NotEmpty String id) {
+        ElasticsearchQueryServiceResponseModel esDocument = elasticsearchQueryService.getDocumentById(id);
+        log.info("Elasticsearch returned document with id {}", id);
+        return ResponseEntity.ok(mapToV2Model(esDocument));
+    }
+
+    @PostMapping("/v1/documents")
     public ResponseEntity<List<ElasticsearchQueryServiceResponseModel>> getDocumentByText(
             @RequestBody @Valid ElasticsearchQueryServiceRequestModel elasticsearchQueryServiceRequestModel) {
         List<ElasticsearchQueryServiceResponseModel> documents = elasticsearchQueryService.getDocumentsByText(
                 elasticsearchQueryServiceRequestModel.getText()
         );
         return ResponseEntity.ok(documents);
+    }
+
+    private ElasticsearchQueryServiceResponseModelV2 mapToV2Model(ElasticsearchQueryServiceResponseModel responseModel) {
+        ElasticsearchQueryServiceResponseModelV2 responseModelV2 = ElasticsearchQueryServiceResponseModelV2.builder()
+                .id(Long.valueOf(responseModel.getId()))
+                .userId(responseModel.getUserId())
+                .text(responseModel.getText())
+                .createdAt(responseModel.getCreatedAt())
+                .build();
+
+        responseModelV2.add(responseModel.getLinks());
+        return responseModelV2;
     }
 }
